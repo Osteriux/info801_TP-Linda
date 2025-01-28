cf2b07fb1b12d8baa9ebbf6259726c0fd0915b26
osteriux <pixave73@gmail.com>
2025-01-28 14:20:54 +0100

    Machine

--- tp_info801/src/main/java/info801/tp/linda/App.java
+++ tp_info801/src/main/java/info801/tp/linda/App.java
@@ -9,6 +9,9 @@ public class App {
 	public static final String METHANE = "methane";
 	public static final String MONOXIDE = "monoxide";
 
+	public static final String POMPE = "pompe";
+	public static final String VENTILATEUR = "ventilateur";
+
 	public static final float SEUIL_EAU = 100;
 	public static final float SEUIL_METHANE = 10;
 	public static final float SEUIL_MONOXIDE = 20;
@@ -19,17 +22,27 @@ public class App {
 		espace.put(METHANE, (float) 5);
 		espace.put(MONOXIDE, (float) 10);
 
+		espace.put(POMPE, false);
+		espace.put(VENTILATEUR, false);
+
 		Capteur capteurEau = new Capteur(EAU, espace, 50.0f);
 		Capteur capteurMethane = new Capteur(METHANE, espace, 5.0f);
 		Capteur capteurMonoxide = new Capteur(MONOXIDE, espace, 10.0f);
 
+		Machine machinePompe = new Machine(POMPE, espace);
+		Machine machineVentilateur = new Machine(VENTILATEUR, espace);
+
 		capteurEau.start();
 		capteurMethane.start();
 		capteurMonoxide.start();
+		machinePompe.start();
+		machineVentilateur.start();
 
 		capteurEau.join();
 		capteurMethane.join();
 		capteurMonoxide.join();
+		machinePompe.join();
+		machineVentilateur.join();
 	}
 
 }
