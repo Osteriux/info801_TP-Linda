9ee88769f70a705a497ae7fb294b8973fd0a06f9
osteriux <pixave73@gmail.com>
2025-02-12 09:27:48 +0100

    add gaz to main

--- drainage2/src/main/java/info801/tp/linda/Commande.java
+++ drainage2/src/main/java/info801/tp/linda/Commande.java
@@ -20,7 +20,7 @@ public class Commande extends Thread {
                 Object[] q_methane = espace.query(new ActualField(App.METHANE), new FormalField(Float.class));
                 Object[] q_monoxide = espace.query(new ActualField(App.MONOXYDE), new FormalField(Float.class));
 
-                if((float) q_methane[1] < App.SEUIL_METHANE_H && (float) q_monoxide[1] < app.SEUIL_METHANE_H){
+                if((float) q_methane[1] < App.SEUIL_METHANE_H && (float) q_monoxide[1] < App.SEUIL_METHANE_H){
                     espace.put(App.POMPE_ACTIVE);
                     espace.put(App.DETECTION_EAU_B);
                     espace.put(App.DETECTION_GAZ_H);
