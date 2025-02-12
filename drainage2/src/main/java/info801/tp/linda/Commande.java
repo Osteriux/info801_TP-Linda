fd90a4af5759b187bbb97d501a6fbe444777de3c
elias-qzo <eliascuzeau@gmail.com>
2025-02-12 09:41:05 +0100

    fix commande

--- drainage2/src/main/java/info801/tp/linda/Commande.java
+++ drainage2/src/main/java/info801/tp/linda/Commande.java
@@ -16,16 +16,20 @@ public class Commande extends Thread {
     public void run() {
         while (true) {
             try {
-                espace.query(new ActualField(App.DETECTION_EAU_H));
+                espace.query(new ActualField(App.EAU_H_DETECTE));
                 Object[] q_methane = espace.query(new ActualField(App.METHANE), new FormalField(Float.class));
                 Object[] q_monoxide = espace.query(new ActualField(App.MONOXYDE), new FormalField(Float.class));
+                float niveau_methane = (float) q_methane[1];
+                float niveau_monoxide = (float) q_monoxide[1];
+                System.out.println(niveau_methane);
+                System.out.println(niveau_monoxide);
 
-                if((float) q_methane[1] < App.SEUIL_METHANE_H && (float) q_monoxide[1] < App.SEUIL_METHANE_H){
+                if(niveau_methane < App.SEUIL_METHANE_H && niveau_monoxide < App.SEUIL_METHANE_H){
                     espace.put(App.POMPE_ACTIVE);
                     espace.put(App.DETECTION_EAU_B);
                     espace.put(App.DETECTION_GAZ_H);
-                    
-                } else if((float) q_methane[1] >= App.SEUIL_METHANE_H || (float) q_monoxide[1] < App.SEUIL_MONOXYDE_H){
+
+                } else if(niveau_methane >= App.SEUIL_METHANE_H || niveau_monoxide < App.SEUIL_MONOXYDE_H){
                     espace.put(App.VENTILATEUR_ACTIVE);
                     espace.put(App.DETECTION_GAZ_B);
                 }
