91f17cb43798aea6ab2be511ac271137f132079f
elias-qzo <eliascuzeau@gmail.com>
2025-02-12 09:43:15 +0100

    In eau haut

--- drainage2/src/main/java/info801/tp/linda/Commande.java
+++ drainage2/src/main/java/info801/tp/linda/Commande.java
@@ -16,13 +16,11 @@ public class Commande extends Thread {
     public void run() {
         while (true) {
             try {
-                espace.query(new ActualField(App.EAU_H_DETECTE));
+                espace.get(new ActualField(App.EAU_H_DETECTE));
                 Object[] q_methane = espace.query(new ActualField(App.METHANE), new FormalField(Float.class));
                 Object[] q_monoxide = espace.query(new ActualField(App.MONOXYDE), new FormalField(Float.class));
                 float niveau_methane = (float) q_methane[1];
                 float niveau_monoxide = (float) q_monoxide[1];
-                System.out.println(niveau_methane);
-                System.out.println(niveau_monoxide);
 
                 if(niveau_methane < App.SEUIL_METHANE_H && niveau_monoxide < App.SEUIL_METHANE_H){
                     espace.put(App.POMPE_ACTIVE);
